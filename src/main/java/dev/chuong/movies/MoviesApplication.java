fade32916e589e32c42723d3459a8a4950c3dc19
Hoang Chuong Pham <pham75@uwindsor.ca>
2026-03-07 16:05:56 -0500

    Commit 0: Add project and configuration environment for the MongoDB

---
+++ src/main/java/dev/chuong/movies/MoviesApplication.java
@@ -0,0 +1,24 @@
+package dev.chuong.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@GetMapping("/root")
+	public String apiIndex() {
+		return "Hello World";
+	}
+
+}
---
+++ src/test/java/dev/chuong/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chuong.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
